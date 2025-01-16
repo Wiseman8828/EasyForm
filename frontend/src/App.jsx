ee6d2843c02bafbe568072f65e68824e21973b38
Ashwini <ashwini.dev.260@gmail.com>
2025-01-16 17:49:21 +0530

    implemented fields page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,35 +7,38 @@ import FieldsPage from "./components/Fields/FieldsPage";
 import FormsPage from "./components/Forms/FormPage";
 import PublicForm from "./components/PublicForms/PublicForms";
 import HomePage from "./components/Home/HomePage";
-import CanActivateRoute  from "./components/Auth/CanActivateRoute"
+import CanActivateRoute from "./components/Auth/CanActivateRoute"
+import { UserProvider } from "./contexts/userContext";
 
-function App () {
+function App() {
     return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route
-                    path="/fields"
-                    element={
-                        <CanActivateRoute>
-                            <FieldsPage />
-                        </CanActivateRoute>
-                    }
-                />
-                <Route
-                    path="/forms"
-                    element={
-                        <CanActivateRoute>
-                            <FormsPage />
-                        </CanActivateRoute>
-                    }
-                />
-                <Route path="/public-form/:formId" element={<PublicForm />} />
-            </Routes>
-        </Router>
+        <UserProvider>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route
+                        path="/fields"
+                        element={
+                            <CanActivateRoute>
+                                <FieldsPage />
+                            </CanActivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/forms"
+                        element={
+                            <CanActivateRoute>
+                                <FormsPage />
+                            </CanActivateRoute>
+                        }
+                    />
+                    <Route path="/public-form/:formId" element={<PublicForm />} />
+                </Routes>
+            </Router>
+        </UserProvider>
     );
 };
 
